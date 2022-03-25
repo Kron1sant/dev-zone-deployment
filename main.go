ad7952c9f7630836e9b65084e619d6c1abd0669f
kron <kron.isant@gmail.com>
2022-03-25 12:29:24 +0300

    remove temp code

--- main.go
+++ main.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-	"context"
 	"devZoneDeployment/api/restful"
 	"devZoneDeployment/config"
-	"devZoneDeployment/db/dom"
 	"devZoneDeployment/db/mongodb"
-	"fmt"
 	"log"
 	"time"
 
 	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
-	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -31,16 +25,12 @@ func main() {
 	restful.AddHandlers(authSite)
 
 	port := config.AppConfig.GetString("service.port")
-	//log.Fatal(http.ListenAndServeTLS(":"+port, "certs/localhost.crt", "certs/localhost.key", router))
+
 	switch gin.Mode() {
 	case "debug":
 		router.Run(":" + port)
 	case "release":
-		log.Fatal(autotls.RunWithManager(router, &autocert.Manager{
-			Prompt:     autocert.AcceptTOS,
-			HostPolicy: autocert.HostWhitelist(""), // ToDo
-			Cache:      autocert.DirCache("certs/.cache"),
-		}))
+		// ToDo
 	case "test":
 		log.Fatal("There is not any tests, yet:(")
 	default:
@@ -58,25 +48,4 @@ func corsHandler() gin.HandlerFunc {
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	})
-}
-
-func test(ds *mongodb.MongoDBSource) {
-	appUsers := ds.Database.Collection("app_users")
-
-	findCursor, err := appUsers.Find(context.TODO(), bson.D{bson.E{"_id", bson.M{"$ne": 8}}})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	res := make([]*dom.User, 0, 1)
-	for findCursor.Next(context.TODO()) {
-		appUser := &dom.User{}
-		if err := findCursor.Decode(appUser); err != nil {
-			log.Fatal(err)
-		}
-		res = append(res, appUser)
-	}
-
-	fmt.Println(res)
-	log.Fatal("asdas")
 }
