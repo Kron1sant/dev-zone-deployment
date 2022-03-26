b0d32871c205f3a0980ef1048ba177b594c70246
kron <kron.isant@gmail.com>
2022-03-26 12:23:52 +0300

    correct the description

--- api/restful/handlers.go
+++ api/restful/handlers.go
@@ -4,24 +4,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Declare all functional handlers.
+// Registers REST api handlers.
 // Such as:
 //	* GET /users - list of application users
-//	* POST /users/:action - add new/edit/delete/setPassword application user
+//	* POST /users/:action - add new/edit/delete/setPassword the application user
 //	* GET /accounts - list of developer accounts
-//	* POST /accounts/:action - add new/edit/delete developer account
+//	* POST /accounts/:action - add new/edit/delete the developer account
+//	* GET /wm - list of virtual machines
+//	* POST /wm/:action - add update/new/edit/delete the virtual machine
 //	* POST /openvpnkey - returns the OpenVPN key for the specified dev account
 func AddHandlers(router gin.IRoutes) {
+	// Users handlers
 	router.GET("/users", GetAppUsers())
-	// action can be add, edit, delete or setPassword
 	router.POST("/users/:action", PostAppUserAction())
-
+	// Accounts handlers
 	router.GET("/accounts", GetDevAccounts())
-	// action can be add, edit or delete
 	router.POST("/accounts/:action", PostDevAccountAction())
-
+	// Virtual machines handlers
 	router.GET("/vm", ListVM())
-	// action can be update
 	router.POST("/vm/:action", PostVMAction())
+	// OpenVPN handlers
 	router.POST("/openvpnkey", PostOpenVPNKey())
 }
