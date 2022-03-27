d9d6ba977d1d68bff35ada219e07c48044798a11
kron <kron.isant@gmail.com>
2022-03-27 21:22:08 +0300

    add type for vm status

--- db/dom/entities.go
+++ db/dom/entities.go
@@ -26,11 +26,26 @@ type DevAccount struct {
 
 // Represent an instance of Virtual machine
 type VM struct {
-	Id            string `bson:"_id" json:"id"`
-	Name          string `bson:"name" json:"name"`
-	HasDevAccount bool   `bson:"hasDevAccount" yaml:"hasDevAccount" json:"hasDevAccount"`
-	DevAccountId  uint   `bson:"devAccountId" yaml:"devAccountId" json:"devAccountId"`
-	Description   string `bson:"description" json:"description"`
-	Params        string `bson:"params" json:"params"`
-	Status        string `bson:"status" json:"status"`
+	Id            string   `bson:"_id" json:"id"`
+	Name          string   `bson:"name" json:"name"`
+	HasDevAccount bool     `bson:"hasDevAccount" yaml:"hasDevAccount" json:"hasDevAccount"`
+	DevAccountId  uint     `bson:"devAccountId" yaml:"devAccountId" json:"devAccountId"`
+	Description   string   `bson:"description" json:"description"`
+	Params        string   `bson:"params" json:"params"`
+	Status        StatusVM `bson:"status" json:"status"`
 }
+
+type StatusVM string
+
+const (
+	VM_STATUS_PROVISIONING StatusVM = "PROVISIONING"
+	VM_STATUS_STARTING     StatusVM = "STARTING"
+	VM_STATUS_RUNNING      StatusVM = "RUNNING"
+	VM_STATUS_STOPPING     StatusVM = "STOPPING"
+	VM_STATUS_STOPPED      StatusVM = "STOPPED"
+	VM_STATUS_RESTARTING   StatusVM = "RESTARTING"
+	VM_STATUS_UPDATING     StatusVM = "UPDATING"
+	VM_STATUS_CRASHED      StatusVM = "CRASHED"
+	VM_STATUS_ERROR        StatusVM = "ERROR"
+	VM_STATUS_DELETING     StatusVM = "DELETING"
+)
