7432a747fc6679a0357e4548e396e612f958435d
kron <kron.isant@gmail.com>
2022-03-27 13:50:20 +0300

    add onepnvpn config setter

--- config/ovpnconf.go
+++ config/ovpnconf.go
@@ -1,16 +1,21 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/spf13/viper"
+)
 
 type ovpnConfig struct {
 	// Path to an easyrsa directory with bin data
-	Easyrsa string
+	Easyrsa string `json:"easyrsa"`
 	// Path to ca cert
-	CaCert string
+	CaCert string `json:"ca-cert"`
 	// Path to tls key
-	TlsKey string
+	TlsKey string `json:"tls-key"`
 	// Path to file with the client key template
-	ClientTemplate string
+	ClientTemplate string `json:"client-template"`
 }
 
 var ovpncfg ovpnConfig
@@ -38,4 +43,10 @@ func GetTlsKeyPath() string {
 
 func GetClientTemplatePath() string {
 	return ovpncfg.ClientTemplate
+}
+
+func SetOpenVPNParamsFromJSON(params string) {
+	if err := json.Unmarshal([]byte(params), &ovpncfg); err != nil {
+		log.Fatalf("set openvpn params from json failed: %s", err)
+	}
 }
